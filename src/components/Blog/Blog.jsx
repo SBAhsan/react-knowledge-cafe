f4a5aa97c86f467e36d0a00aeea6ed630a64845b
SBAhsan <ahsangustavo90@gmail.com>
2024-03-16 19:42:13 +0600

    added bookmarks and bookmark.jsx and linked them with app,blogs,blog.jsx with useState and event handler

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,38 @@
+import { space } from 'postcss/lib/list';
 import PropTypes from 'prop-types';
+import { BsBookmarks } from "react-icons/bs";
 
-const Blog = ({ blog }) => {
-    console.log(blog);
+const Blog = ({ blog, handleAddToBookmarks }) => {
+    const { author_img, author, title, cover_img, reading_time, posted_date, hashtags } = blog;
     return (
-        <div>
-
+        <div className='mb-8 border-b-2 pb-8'>
+            <img className='w-full mb-8' src={cover_img} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex gap-5 items-center pb-6'>
+                    <img className='w-16 h-16 rounded-full' src={author_img} alt="" />
+                    <div>
+                        <b><h3 className="text-xl">{author}</h3></b>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span className='mr-2'>{reading_time} min read</span>
+                    <button onClick={() => handleAddToBookmarks(blog)}><BsBookmarks></BsBookmarks></button>
+                </div>
+            </div>
+            <b><h2 className="text-4xl mb-8">{title}</h2></b>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a className='ml-2' href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
 Blog.propTypes = {
-    blog : PropTypes.object.isRequired
+    blog: PropTypes.object.isRequired,
+    handleAddToBookmarks: PropTypes.func
 }
 
 export default Blog;
--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,19 @@
+import Bookmark from "../Bookmark/Bookmark";
+import PropTypes from 'prop-types';
 
-
-const Bookmarks = () => {
+const Bookmarks = ({ bookmarks }) => {
     return (
-        <div className="md:w-1/3">
-            <h2>Bookmarks</h2>
+        <div className="md:w-1/3 bg-slate-400 ml-2 mt-4 p-4 my-auto rounded-md">
+            <h2 className="text-4xl text-center">Bookmarked Blogs : {bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+            }
         </div>
     );
 };
+
+Bookmarks.propTypes = {
+    bookmarks: PropTypes.array
+}
 
 export default Bookmarks;
